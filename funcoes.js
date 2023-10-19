660b60c09a371f3682ca7713058aa20bb9984367
EuNicolasCastro <98694704+EuNicolasCastro@users.noreply.github.com>
2023-10-19 19:40:01 -0300

    ajeitando css e incluindo div

---
+++ funcoes.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('str0').forEach(e=>{
+    e.addEventListener('mouseover', function(){
+        window.onmousemove=function(j){
+            x=clientX
+            y=clientY
+            document.getElementById
+        }
+    })
+})
