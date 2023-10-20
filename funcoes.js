4f996aca66c26c277dbc8bc456b6137accf6eabe
Nícolas Castro <98694704+EuNicolasCastro@users.noreply.github.com>
2023-10-20 13:38:12 -0300

    Update funcoes.js

--- funcoes.js
+++ funcoes.js
@@ -3,7 +3,16 @@ document.querySelectorAll('str0').forEach(e=>{
         window.onmousemove=function(j){
             x=clientX
             y=clientY
-            document.getElementById
+            document.getElementById("nome").style.top = y-30+"px"
+            document.getElementById("nome").style.left = x+10+"px"
         }
+        e.style.fill="#345678"
+        document.getElementById("nomep").innerText = e.id
+        document.getElementById("nome").style.opacity=1
     })
+    e.addEventListener("mouseleave", function(){
+        e.style.fill="#123456"
+        document.getElementById("nome").style.opacity=0
+    })
+    
 })
