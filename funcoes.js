4c82ca4d0fd70a9c2c4c088d8016f55987e06363
EuNicolasCastro <98694704+EuNicolasCastro@users.noreply.github.com>
2023-10-20 18:18:49 -0300

    atualizando tudo

--- funcoes.js
+++ funcoes.js
@@ -1,18 +1,21 @@
-document.querySelectorAll('str0').forEach(e=>{
-    e.addEventListener('mouseover', function(){
-        window.onmousemove=function(j){
-            x=clientX
-            y=clientY
-            document.getElementById("nome").style.top = y-30+"px"
-            document.getElementById("nome").style.left = x+10+"px"
-        }
-        e.style.fill="#345678"
-        document.getElementById("nomep").innerText = e.id
-        document.getElementById("nome").style.opacity=1
+document.querySelectorAll('.str0').forEach(e=>{
+        e.addEventListener("mouseover", function(){
+            window.onmousemove=function(j){
+                x=j.clientX
+                y=j.clientY
+                document.getElementById("nome").style.top=y-30+"px"
+                document.getElementById("nome").style.left=x+10+"px"
+                
+            }
+            e.style.fill="pink"
+            document.getElementById("nome").style.opacity=1
+            document.getElementById("nomep").innerText=e.id
+            
+        })
+
+        e.addEventListener("mouseleave", function(){
+            e.style.fill="#c5c5c5"
+            document.getElementById("nome").style.opacity=0
+        })
+        
     })
-    e.addEventListener("mouseleave", function(){
-        e.style.fill="#123456"
-        document.getElementById("nome").style.opacity=0
-    })
-    
-})
